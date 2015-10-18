2e74001489c7e2398cc1fbcecf97bed36696894d
Rosario B. <rossa_4057@hotmail.com>
2015-10-18 20:32:29 +0200

    Understanding Routing 2

--- Donations/App_Start/RouteConfig.cs
+++ Donations/App_Start/RouteConfig.cs
@@ -10,30 +10,33 @@ namespace Donations {
 		public static void RegisterRoutes(RouteCollection routes) {
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.MapMvcAttributeRoutes();
+			//routes.MapMvcAttributeRoutes();
 
 			routes.MapRoute(
 				name: "Default",
-				url: "{controller}/{action}"
+				url: "{controller}/{action}",
+				defaults: new { controller = "Donation", action = "Index" }
 			);
 
 			routes.MapRoute(
-				name: "Donation_Index_Id",
-				url: "{controller}/{action}/{id}"
-				//defaults: new { controller = "Movies", action = "Detail", id = UrlParameter.Optional }
+				name: "Donnation_Welcome_Name_NumTimes",
+				url: "{controller}/{action}/{name}/{numTimes}",
+				defaults: new { action = "Welcome", name = UrlParameter.Optional, numTimes = UrlParameter.Optional },
+				constraints: new { controller = "Donation" }
 			);
 
-			//routes.MapRoute(
-			//	name: "SingleName",
-			//	url: "{controller}/{action}/{name}",
-			//	defaults: new { controller = "Donation", action = "Welcome" }
-			//);
+			routes.MapRoute(
+				name: "Movies_Action_Id",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Movies", action = "Detail", id = UrlParameter.Optional }
+			);
 
-			//routes.MapRoute(
-			//	name: "Welcomeeee",
-			//	url: "{controller}/{action}/{name}/{numTimes}",
-			//	defaults: new { controller = "Donation", action = "Welcome" }
-			//);
+			routes.MapRoute(
+				name: "Last_Chance",
+				url: "{controller}/{action}",
+				defaults: new { },
+				constraints: new { controller = "Donation", action = "Index" }
+			);
 		}
     }
 }
--- Donations/Controllers/DonationController.cs
+++ Donations/Controllers/DonationController.cs
@@ -2,16 +2,13 @@
 
 namespace Donations.Controllers {
 
-	[RoutePrefix("Donation")]
 	public class DonationController : Controller {
 
-        [Route("Index")]
         public ActionResult Index() {
             return View();
         }
 
-		[Route("Welcome/{name?}/{numTimes?}")]
-		public ActionResult Welcome(string name = "Guest", int? numTimes = 1) {
+		public ActionResult Welcome(string name = "Guest", int numTimes = 1) {
             ViewBag.Message = "Hello " + name;
             ViewBag.NumTimes = numTimes;
 
